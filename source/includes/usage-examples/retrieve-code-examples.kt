f27ab8f688b1f74e1590373c441878c70fc7fbe9
Michael Morisi <michael.morisi@mongodb.com>
2024-07-05 13:32:49 -0400

    DOCSP-41132: Read page

---
+++ source/includes/usage-examples/retrieve-code-examples.kt
@@ -0,0 +1,37 @@
+// start-find
+val filter = <filter>
+val results = collection.find(filter)
+results.forEach {
+    print(it)
+}
+// end-find
+
+// start-count-all
+val count = collection.countDocuments()
+print(count)
+// end-count-all
+
+// start-count-query
+val filter = <filter>
+val count = collection.countDocuments(filter)
+print(count)
+// end-count-query
+
+// start-estimated-count
+val count = collection.estimatedDocumentCount()
+print(count)
+// end-estimated-count
+
+// start-distinct
+val distinctResults = collection.distinct("<field name>")
+distinctResults.forEach { result ->
+    print(result)
+}
+// end-distinct
+
+// start-watch
+val changeStream = collection.watch()
+changeStream.forEach { changeEvent ->
+    print(changeEvent)
+}
+// end-watch
