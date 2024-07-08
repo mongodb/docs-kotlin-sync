356d8887dc43c020fba8f5b8db3c5f1a0e5c9b48
Michael Morisi <michael.morisi@mongodb.com>
2024-07-08 11:25:49 -0400

    Address feedback

--- source/includes/usage-examples/sample-app.kt
+++ source/includes/usage-examples/sample-app.kt
@@ -3,6 +3,9 @@ import com.mongodb.MongoClientSettings
 import com.mongodb.kotlin.client.*
 import com.mongodb.client.model.Filters.*
 
+// Update with the object representation of your collection's documents
+data class Document(val id: String)
+
 fun main() {
     val uri = "<connection string URI>"
     
@@ -14,7 +17,7 @@ fun main() {
     // Create a new client and connect to the server
     val mongoClient = MongoClient.create(settings)
     val database = mongoClient.getDatabase("<database name>")
-    val collection = database.getCollection<Movie>("<collection name>")
+    val collection = database.getCollection<Document>("<collection name>")
 
     // Start example code here
 
