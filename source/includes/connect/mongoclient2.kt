ab30e3dbf9b29176dd5509f8c573af19be56b02c
Lindsey Moore <lindsey.moore@mongodb.com>
2024-08-14 16:13:51 -0400

    new option for connecting

--- source/includes/connect/mongoclient2.kt
+++ source/includes/connect/mongoclient2.kt
@@ -1,29 +1,16 @@
 import com.mongodb.*
 import com.mongodb.kotlin.client.MongoClient
-import org.bson.BsonInt64
-import org.bson.Document
 
 fun main() {
     
-    // start-connect-to-atlas
     // start-connect-to-atlas-w-uri
     // Replace the placeholder with your Atlas connection string
     val uri = "<connection string>"
-    val mongoClient1 = MongoClient.create(uri)
-    // end-connect-to-atlas-w-uri
-    
-    // Construct a ServerApi instance using the ServerApi.builder() method
-    val serverApi = ServerApi.builder()
-        .version(ServerApiVersion.V1)
-        .build()
-    val settings = MongoClientSettings.builder()
-        .applyConnectionString(ConnectionString(uri))
-        .serverApi(serverApi)
-        .build()
 
     // Create a new client and connect to the server
-    val mongoClient = MongoClient.create(settings)
+    val mongoClient = MongoClient.create(uri)
     val database = mongoClient.getDatabase("sample_mflix")
+    // end-connect-to-atlas-w-uri
 
     try {
         // Send a ping to confirm a successful connection
@@ -33,5 +20,4 @@ fun main() {
     } catch (me: MongoException) {
         System.err.println(me)
     }
-    // end-connect-to-atlas
 }
