d57a4cf5230fbbfba9d3abc4f34a90b0783d7d43
Stephanie Aurelio <stephanie.aurelio@mongodb.com>
2024-09-10 10:40:32 -0700

    implement RR feedback

--- source/includes/write/transaction.kt
+++ source/includes/write/transaction.kt
@@ -1,8 +1,11 @@
 import com.mongodb.*
 import com.mongodb.client.ClientSession
 import com.mongodb.client.MongoClients
-import com.mongodb.client.MongoCollection
 import org.bson.Document
+
+// start-data-class
+data class Restaurant(val name: String, val cuisine: String)
+// end-data-class
 
 fun main() {
 // start-transaction
@@ -10,23 +13,24 @@ fun main() {
     val client = MongoClients.create("<connection string>")
 
     // Gets the database and collection
-    val restaurantsDb = client.getDatabase("sample_restaurants")
-    val restaurantsCollection: MongoCollection<Document> = restaurantsDb.getCollection("restaurants")
+    val database = client.getDatabase("sample_restaurants")
+    val collection = database.getCollection<Restaurant>("restaurants")
     
-    fun insertDocuments(session: ClientSession) {
+    // Defines options and inserts restaurants into the collection
+    fun insertRestaurantsInTransaction(session: ClientSession) {
         // Sets options for the collection operation within the transaction
-        val restaurantsCollectionWithOptions = restaurantsCollection
+        val restaurantsCollectionWithOptions = collection
             .withWriteConcern(WriteConcern("majority"))
             .withReadConcern(ReadConcern.LOCAL)
 
-        // Inserts documents within the transaction
+        // Inserts restaurants within the transaction
         restaurantsCollectionWithOptions.insertOne(
             session,
-            Document("name", "Kotlin Sync Pizza").append("cuisine", "Pizza")
+            Restaurant("name", "Kotlin Sync Pizza").append("cuisine", "Pizza")
         )
         restaurantsCollectionWithOptions.insertOne(
             session,
-            Document("name", "Kotlin Sync Burger").append("cuisine", "Burger")
+            Restaurant("name", "Kotlin Sync Burger").append("cuisine", "Burger")
         )
     }
 
@@ -40,7 +44,7 @@ fun main() {
 
             // Uses the withTransaction method to start a transaction and run the given function
             session.withTransaction({
-                insertDocuments(session)
+                insertRestaurantsInTransaction(session)
                 println("Transaction succeeded")
             }, txnOptions)
         } catch (e: Exception) {
