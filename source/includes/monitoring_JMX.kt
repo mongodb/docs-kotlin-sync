826abec5d89941f0061e8a86f8bcfcf1d7f6ad06
Rachel Mackintosh <rachel.mackintosh@mongodb.com>
2025-01-28 16:13:38 -0500

    monitoring page

---
+++ source/includes/monitoring_JMX.kt
@@ -0,0 +1,30 @@
+import com.mongodb.kotlin.client.MongoClient
+import org.bson.json.JsonWriterSettings
+import com.mongodb.event.*
+import com.mongodb.MongoClientSettings
+import com.mongodb.ConnectionString
+import com.mongodb.management.JMXConnectionPoolListener
+
+
+fun main() {
+    val uri = "<connection string uri>"
+
+// Instantiate your JMX listener
+    val connectionPoolListener = JMXConnectionPoolListener()
+
+// Include the listener in your client settings
+    val settings = MongoClientSettings.builder()
+        .applyConnectionString(ConnectionString(uri))
+        .addCommandListener(connectionPoolListener)
+        .build()
+
+// Pause execution
+    try {
+        println("Navigate to JConsole to see your connection pools...")
+        Thread.sleep(Long.MAX_VALUE)
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+
+    mongoClient.close()
+}
