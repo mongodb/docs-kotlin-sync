0b3db3ea9ccd7e793582e2fc225a6aa08bc38e4f
Rachel Mackintosh <rachel.mackintosh@mongodb.com>
2025-04-03 10:04:04 -0400

    RR feedback

--- source/includes/data-formats/serialization.kt
+++ source/includes/data-formats/serialization.kt
@@ -105,6 +105,10 @@ fun main() {
     val myCustomCodec = KotlinSerializerCodec.create<PaintOrder>(
         bsonConfiguration = BsonConfiguration(bsonNamingStrategy = BsonNamingStrategy.SNAKE_CASE)
     )
+
+    val registry = CodecRegistries.fromRegistries(
+        CodecRegistries.fromCodecs(myCustomCodec), collection.codecRegistry
+    )
     // end-snake-case
 
     // start-poly-operations
